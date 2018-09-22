2dcd37b386451d9f90671487fb188334346766e0
uoon-dev <uoon.dev@gmail.com>
2018-09-22 18:07:11 +0900

    make mUI odules for each ccommon coponents and fix styles

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,38 +1,77 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView, Image } from 'react-native';
 import { connect } from 'react-redux';
 
-import PlaceInput from '../../components/PlaceInput/PlaceInput';
 import { addPlace } from '../../store/actions/index';
+import PlaceInput from '../../components/PlaceInput/PlaceInput';
+import MainText from '../../components/UI/MainText/MainText';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import PickImage from '../../components/PickImage/PickImage';
+import PickLocation from '../../components/PickLocation/PickLocation';
 
 class SharePlaceScreen extends Component {
-    constructor(props) {
-      super(props);
-      this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
-    }
+  state = {
+    placeName: ""
+  }
 
-    onNavigatorEvent = event => {
-      if (event.type === "NavBarButtonPress") {
-        if (event.id === "sideDrawerToggle") {
-          this.props.navigator.toggleDrawer({
-            side: "left"
-          })
-        }
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
+  onNavigatorEvent = event => {
+    if (event.type === "NavBarButtonPress") {
+      if (event.id === "sideDrawerToggle") {
+        this.props.navigator.toggleDrawer({
+          side: "left"
+        })
       }
     }
+  }
 
-    placeAddedHandler = placeName => {
-        this.props.onAddPlace(placeName);
-    }
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  }
 
-    render () {
-        return (
-            <View>
-                <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-            </View>
-        );
+  placeAddedHandler = () => {
+    if (this.state.placeName.trim() !== "") {
+      this.props.onAddPlace(this.state.placeName);
     }
+  }
+
+  render () {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Share a Place with us!</HeadingText>
+          </MainText>
+          <PickImage></PickImage>
+          <PickLocation></PickLocation>
+          <PlaceInput 
+            placeName={this.state.placeName} 
+            onChangedText={this.placeNameChangedHandler}>
+          </PlaceInput>
+          <View style={styles.button}>
+            <Button title="Share the Place!" onPress={this.placeAddedHandler}></Button>
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+  button: {
+    margin: 8
+  }
+})
 
 const mapDispatchToProps = dispatch => {
     return {
